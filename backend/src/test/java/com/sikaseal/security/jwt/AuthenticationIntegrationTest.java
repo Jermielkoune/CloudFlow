b6bb774d318e4417a3c8b1ce6bc5257d7301682b
KOUNOUHO Kpessou Jermiel <kpessou-jermiel.kounouho@epitech.eu>
2025-09-11 19:41:46 +0200

    Add JWT configuration properties for testing in AuthenticationIntegrationTest

--- backend/src/test/java/com/sikaseal/security/jwt/AuthenticationIntegrationTest.java
+++ backend/src/test/java/com/sikaseal/security/jwt/AuthenticationIntegrationTest.java
@@ -12,6 +12,7 @@ import java.lang.annotation.Target;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import tech.jhipster.config.JHipsterProperties;
 
 @Target(ElementType.TYPE)
@@ -27,12 +28,9 @@ import tech.jhipster.config.JHipsterProperties;
     }
 )
 @WebFluxTest(
-    controllers = { AuthenticateController.class },
-    properties = {
-        "jhipster.security.authentication.jwt.base64-secret=fd54a45s65fds737b9aafcb3412e07ed99b267f33413274720ddbb7f6c5e64e9f14075f2d7ed041592f0b7657baf8",
-        "jhipster.security.authentication.jwt.token-validity-in-seconds=60000",
-    }
+    controllers = { AuthenticateController.class }
 )
-@ComponentScan({})
+@ActiveProfiles("test") //
+@ComponentScan()
 public @interface AuthenticationIntegrationTest {
 }
